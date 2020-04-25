ea11626b702ba460b8c24f3aaf4b8ba8e9219956
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-25 08:24:34 -0700

    postgres ver 7

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -16,7 +16,7 @@ public class SqlAppl
     private void testDatabase() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/contactdb";
+            String url = "jdbc:postgresql://localhost:5432/postgres";
             String login = "postgres";
             String password = "postgres";
             Connection con = DriverManager.getConnection(url, login, password);
