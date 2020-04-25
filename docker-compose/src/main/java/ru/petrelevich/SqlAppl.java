328c0e586eb25d0c9442f35f2833c3730b74acab
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-25 07:47:31 -0700

    postgres ver

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -1,52 +1,34 @@
-package ru.petrelevich;
+package SqlAppl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class SqlAppl {
-    public static void main(String[] args) throws SQLException {
-        System.out.println("application is starting...");
-        new SqlAppl().start();
-        System.out.println("application finished");
-    }
+public class JavaPostgreSqlVersion {
 
-    private void start() throws SQLException {
-        try(Connection connection = makeConnection()) {
-            System.out.println(connection.isValid(1));
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String user = "postgres";
+        String password = "password";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("SELECT VERSION()")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+        
+            Logger lgr = Logger.getLogger(JavaPostgreSqlVersion.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
     }
-
-    private Connection makeConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost/postgres";
-        Properties props = new Properties();
-        props.setProperty("user","postgres");
-        props.setProperty("password","password");
-        props.setProperty("ssl","false");
-		Statement st = null;
-        ResultSet rs = null;
-        try{
-            Class.forName("org.postgresql.Driver");
-            System.out.println("connect");
-            } catch (ClassNotFoundException cnfe){
-              System.out.println("Could not find the JDBC driver!");
-              System.exit(1);
-            }
-        Connection conn = null;
-        try {
-            //database location, database user, database password
-            conn = DriverManager.getConnection("jdbc:postgresql:POS_DB","postgres", "password");
-            st = conn.createStatement();
-            String qs = "CREATE TABLE IF NOT EXISTS user(user_id SERIAL NOT NULL PRIMARY KEY,username varchar(225) NOT NULL UNIQUE,password varchar(225),islogged varchar(10))";
-            String qs1 = "SELECT * FROM test";
-        rs = st.executeQuery(qs);
-
-            System.out.println("connect");
-             } catch (SQLException sqle) {
-               System.out.println("Could not connect");
-               System.exit(1);
-             }
-        return DriverManager.getConnection(url, props);
-    }
+}
 }
