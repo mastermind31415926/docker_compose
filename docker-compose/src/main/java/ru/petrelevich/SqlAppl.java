7e1e7a1f0f29f0808b02458b2dfa217317bc63ac
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-25 08:34:10 -0700

    postgres ver 8

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -18,14 +18,14 @@ public class SqlAppl
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/postgres";
             String login = "postgres";
-            String password = "postgres";
+            String password = "password";
             Connection con = DriverManager.getConnection(url, login, password);
             try {
                 Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery("select * from information_schema.tables");
                 while (rs.next()) {
-                    String str = rs.getString("contact_id") + ":" + rs.getString(1);
-                    System.out.println("Contact:" + str);
+                    String str = rs.getString("table_name") + ":" + rs.getString(2);
+                    System.out.println("table_name:" + str);
                 }
                 rs.close();
                 stmt.close();
