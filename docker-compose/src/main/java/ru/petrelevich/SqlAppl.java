0aeddaad75295b110e2f111280d9d137c83ba3e1
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-25 07:37:02 -0700

    create table

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -24,6 +24,30 @@ public class SqlAppl {
         props.setProperty("user","postgres");
         props.setProperty("password","password");
         props.setProperty("ssl","false");
+		Statement st = null;
+        ResultSet rs = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            System.out.println("connect");
+            } catch (ClassNotFoundException cnfe){
+              System.out.println("Could not find the JDBC driver!");
+              System.exit(1);
+            }
+        Connection conn = null;
+        try {
+            //database location, database user, database password
+            conn = DriverManager.getConnection
+                           ("jdbc:postgresql:"POS_DB","postgres", "password");
+            st = conn.createStatement();
+            String qs = "CREATE TABLE IF NOT EXISTS user(user_id SERIAL NOT NULL PRIMARY KEY,username varchar(225) NOT NULL UNIQUE,password varchar(225),islogged varchar(10))";
+            String qs1 = "SELECT * FROM test";
+        rs = st.executeQuery(qs);
+
+            System.out.println("connect");
+             } catch (SQLException sqle) {
+               System.out.println("Could not connect");
+               System.exit(1);
+             }
         return DriverManager.getConnection(url, props);
     }
 }
