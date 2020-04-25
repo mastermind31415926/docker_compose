c5d5f05089f7fcb3817438479340808ec3e3471b
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-25 08:23:03 -0700

    postgres ver 6

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -1,34 +1,39 @@
 package ru.petrelevich;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class SqlAppl {
-
+ 
+public class SqlAppl 
+{
     public static void main(String[] args) {
-
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "password";
-
-        try (Connection con = DriverManager.getConnection(url, user, password);
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("SELECT VERSION()")) {
-
-            if (rs.next()) {
-                System.out.println(rs.getString(1));
+        SqlAppl m = new SqlAppl();
+        m.testDatabase();
+    }
+ 
+    private void testDatabase() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/contactdb";
+            String login = "postgres";
+            String password = "postgres";
+            Connection con = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("select * from information_schema.tables");
+                while (rs.next()) {
+                    String str = rs.getString("contact_id") + ":" + rs.getString(1);
+                    System.out.println("Contact:" + str);
+                }
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
             }
-
-        } catch (SQLException ex) {
-        
-            Logger lgr = Logger.getLogger(SqlAppl.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
-
