57c67d35fcfb3d2bfa38848205e2f58ce2f19ffc
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-04-30 03:11:00 -0700

    host modified

--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -16,9 +16,9 @@ public class SqlAppl
     private void testDatabase() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String url = "jdbc:postgresql://llocalhost:5432/postgres";
             String login = "postgres";
-            String password = "passwor11111d";
+            String password = "password";
             Connection con = DriverManager.getConnection(url, login, password);
             try {
                 Statement stmt = con.createStatement();
