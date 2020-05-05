7d73939c602152611fc3f494ba528cc7f955440d
Oleksandr Markusyk <mastermind31415926@gmail.com>
2020-05-05 11:56:23 -0700

    eternal task

--- docker-compose/scripts/make-app.mk
+++ docker-compose/scripts/make-app.mk
@@ -1,7 +1,7 @@
 USER = "$(shell id -u):$(shell id -g)"
 
 app:
-	docker-compose up --abort-on-container-exit
+	docker-compose up
 
 app-build:
 	docker-compose build
--- docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
+++ docker-compose/src/main/java/ru/petrelevich/SqlAppl.java
@@ -16,13 +16,13 @@ public class SqlAppl
     private void testDatabase() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://llocalhost:5432/postgres";
+            String url = "jdbc:postgresql://localhost:5432/postgres";
             String login = "postgres";
             String password = "password";
             Connection con = DriverManager.getConnection(url, login, password);
             try {
                 Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery("select tables from information_schema.tables");
+                ResultSet rs = stmt.executeQuery("select * from information_schema.tables");
                 while (rs.next()) {
                     String str = rs.getString("table_name") + ":" + rs.getString(2);
                     System.out.println("table_name:" + str);
